cfe37379f9c59b4979277545c3b3ba54e7ae13c6
Agata Firlejczyk <afirlejczyk@divante.pl>
2020-02-13 20:38:59 +0100

    Fix saving selected attributes. Save only one option for 'All attributes' selection.

---
+++ src/module-vsbridge-indexer-catalog/Model/Config/Backend/ProductAttributes.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package  Divante\VsbridgeIndexerCatalog
+ * @author Agata Firlejczyk <afirlejczyk@divante.pl>
+ * @copyright 2019 Divante Sp. z o.o.
+ * @license See LICENSE_DIVANTE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Divante\VsbridgeIndexerCatalog\Model\Config\Backend;
+
+use Magento\Framework\App\Config\Value;
+
+/**
+ * Class ProductAttributes
+ */
+class ProductAttributes extends Value
+{
+    /**
+     * @inheritdoc
+     */
+    public function beforeSave()
+    {
+        if (is_array($this->getValue())) {
+            if (in_array('', $this->getValue())) {
+                $this->setValue('');
+            }
+        }
+
+        return parent::beforeSave();
+    }
+}
