95da6e6f8dd9ad859f4c4e39c71a83597e58aba6
Ha The Tung <tunght13488@gmail.com>
2020-11-30 15:09:13 +0800

    invalidate cache on reindexing

--- src/module-vsbridge-indexer-core/Indexer/Action/Rows.php
+++ src/module-vsbridge-indexer-core/Indexer/Action/Rows.php
@@ -3,11 +3,41 @@
 
 namespace Divante\VsbridgeIndexerCore\Indexer\Action;
 
+use Divante\VsbridgeIndexerCore\Cache\Processor;
+use Divante\VsbridgeIndexerCore\Indexer\GenericIndexerHandlerFactory;
+use Divante\VsbridgeIndexerCore\Indexer\RebuildActionPool;
+use Divante\VsbridgeIndexerCore\Indexer\StoreManager;
+
 /**
  * Rows reindex action
  */
 class Rows extends AbstractAction
 {
+    /**
+     * @var Processor
+     */
+    private $cacheProcessor;
+
+    /**
+     * Rows constructor.
+     *
+     * @param RebuildActionPool $actionPool
+     * @param GenericIndexerHandlerFactory $indexerHandlerFactory
+     * @param StoreManager $storeManager
+     * @param Processor $cacheProcessor
+     * @param string $typeName
+     */
+    public function __construct(
+        RebuildActionPool $actionPool,
+        GenericIndexerHandlerFactory $indexerHandlerFactory,
+        StoreManager $storeManager,
+        Processor $cacheProcessor,
+        string $typeName
+    ) {
+        parent::__construct($actionPool, $indexerHandlerFactory, $storeManager, $typeName);
+        $this->cacheProcessor = $cacheProcessor;
+    }
+
     /**
      * Execute rows reindex
      *
@@ -22,6 +52,7 @@ class Rows extends AbstractAction
         foreach ($stores as $store) {
             $this->getIndexerHandler()->saveIndex($this->rebuild((int) $store->getId(), $ids), $store);
             $this->getIndexerHandler()->cleanUpByTransactionKey($store, $ids);
+            $this->cacheProcessor->cleanCacheByDocIds($store->getId(), $this->getIndexerHandler()->getTypeName(), $ids);
         }
     }
 }
